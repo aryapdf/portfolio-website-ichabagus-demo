070b3876eed5a3642a19d3cb0913dbb725353b29
Arya Agni Pradana Firdaus <aryaagnipradana@gmail.com>
2024-02-17 10:45:21 +0700

    add navbar feature

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: ["index.html"],
   theme: {
     container: {
-        center: true,
-        padding: '16px',
+      center: true,
+      padding: "16px",
     },
     extend: {
-        colors: {
-            primary: '#00A9FF',
-            secondary: '#89CFF3',
-            third: '#A0E9FF',
-            fourth: '#CDF5FD'
-
-        },
-        screens: {
-            '2xl': '1320px'
-        }
+      colors: {
+        primary: "#00A9FF",
+        secondary: "#89CFF3",
+        third: "#A0E9FF",
+        fourth: "#CDF5FD",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+      fontFamily: {
+        montserrat: ["Montserrat", 'sans-serif'],
+      },
     },
   },
   plugins: [],
-}
-
+};
